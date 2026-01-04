e01b24f3af8e9dac5b4d3d6745a5dbd48c96fa5a
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2026-01-04 09:32:18 +0100

    M6: implement recycling (recycle-to/recycle2) + tests

--- src/recycle.lisp
+++ src/recycle.lisp
@@ -2,12 +2,41 @@
 
 (in-package #:cl-vctrs-lite)
 
+(defun broadcast-scalar (x n)
+  "Return a simple-vector of length N filled with X."
+  (let ((out (make-array n)))
+    (dotimes (i n) (setf (aref out i) x))
+    out))
+
 (defun recycle-to (col n &key (name "value"))
-  "Recycle COL to length N using strict rules. (stub for M6)"
-  (declare (ignore col n name))
-  (%simple-error "recycle-to: not implemented yet"))
+  "Recycle COL to length N using strict rules.
+Scalar means non-vector-like or vector-like of length 1.
+Returns a simple-vector."
+  (%ensure-nonnegative-integer n :name "n")
+  (let* ((vec-like (%vector-like-p col))
+         (len (if vec-like (col-length col) 1)))
+    (cond
+      ;; already desired length
+      ((= len n)
+       (if vec-like (as-col col) (broadcast-scalar col n)))
+      ;; length-1 broadcast (including scalar)
+      ((= len 1)
+       (let ((val (if vec-like (col-ref (as-col col) 0) col)))
+         (broadcast-scalar val n)))
+      (t
+       (%simple-error "recycle-to: cannot recycle ~a of length ~a to length ~a"
+                      name len n)))))
 
 (defun recycle2 (a b &key (name-a "a") (name-b "b"))
-  "Recycle A and B to a common size. (stub for M6)"
-  (declare (ignore a b name-a name-b))
-  (%simple-error "recycle2: not implemented yet"))
+  "Recycle A and B to a common size with strict rules.
+Returns three values (A2 B2 N)."
+  (let* ((va (%vector-like-p a))
+         (vb (%vector-like-p b))
+         (na (if va (col-length a) 1))
+         (nb (if vb (col-length b) 1)))
+    (when (and (> na 1) (> nb 1) (/= na nb))
+      (%simple-error "recycle2: incompatible lengths ~a and ~a" na nb))
+    (let* ((n (max na nb))
+           (a2 (recycle-to a n :name name-a))
+           (b2 (recycle-to b n :name name-b)))
+      (values a2 b2 n))))
--- test/recycle-tests.lisp
+++ test/recycle-tests.lisp
@@ -2,5 +2,45 @@
 
 (in-package #:cl-vctrs-lite/test)
 
-(test scaffold-recycle-placeholder
-  (is t))
+;; M6 — Recycling tests
+
+(test recycle-to-broadcast-scalar
+  (is (equalp (recycle-to 7 4) #(7 7 7 7)))
+  (is (equalp (recycle-to "x" 2) #("x" "x"))))
+
+(test recycle-to-len1-vector
+  (is (equalp (recycle-to #(5) 3) #(5 5 5)))
+  (is (equalp (recycle-to '(5) 3) #(5 5 5))))
+
+(test recycle-to-same-length
+  (is (equalp (recycle-to #(1 2 3) 3) #(1 2 3))))
+
+(test recycle-to-error-message
+  (let* ((bad #(1 2))
+         (err (handler-case
+                   (progn (recycle-to bad 3 :name "x") nil)
+                 (error (e) e))))
+    (is err)
+    (let ((msg (princ-to-string err)))
+      (is (search "recycle" msg))
+      (is (search "x" msg))
+      (is (search "2" msg))
+      (is (search "3" msg)))))
+
+(test recycle2-broadcast-and-length
+  (multiple-value-bind (a2 b2 n)
+      (recycle2 #(1 2 3) 10)
+    (is (= n 3))
+    (is (equalp a2 #(1 2 3)))
+    (is (equalp b2 #(10 10 10)))))
+
+(test recycle2-incompatible-lengths-error
+  (let ((err (handler-case
+                 (progn (recycle2 #(1 2) #(1 2 3)) nil)
+               (error (e) e))))
+    (is err)
+    (let ((msg (princ-to-string err)))
+      (is (search "incompatible" msg))
+      (is (search "length" msg))
+      (is (search "2" msg))
+      (is (search "3" msg)))) )
